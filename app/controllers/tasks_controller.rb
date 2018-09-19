dedbfae8ca0a99761641ec625797533df7940436
KatherineJF <kjfitzpat@hotmail.com>
2018-09-18 19:29:44 -0700

    wave 2 and form created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,17 @@ class TasksController < ApplicationController
     id = params[:id]
     @task = Task.find(id)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save 
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  # resources :tasks, only: [:index, :show]
+  root 'tasks#index'
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  resources :tasks, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

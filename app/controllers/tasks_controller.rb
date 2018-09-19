d9a578187ef1b25b90edc9e23bbe4c72cd6d42b5
KatherineJF <kjfitzpat@hotmail.com>
2018-09-18 18:41:48 -0700

    fixed show bug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,9 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-end
 
-def show
-  id = params[:id]
-  @task = Task.find(id)
+  def show
+    id = params[:id]
+    @task = Task.find(id)
+  end
 end

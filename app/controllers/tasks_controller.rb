6d5b763d2d0c3f7703c27f59068a723efb82e711
KatherineJF <kjfitzpat@hotmail.com>
2018-09-21 23:52:44 -0700

    wave 3 done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,43 @@
 
-
-class TasksController < ApplicationController
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-    id = params[:id]
-    @task = Task.find(id)
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
-    if @task.save 
-      redirect_to root_path
-    else
-      render :new
+  class TasksController < ApplicationController
+    def index
+      @tasks = Task.all
     end
+
+    def show
+      id = params[:id]
+      @task = Task.find_by(id:id)
+    end
+
+    def new
+      @task = Task.new
+    end
+
+    def create
+      @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+      if @task.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @task = Task.find(params[:id].to_i)
+    end
+
+    def update
+      @task = Task.find(params[:id])
+      @task.update(name: params[:task][:name], description: params[:task][:description])
+      task_path(@task)
+      redirect_to root_path
+    end
+
+    def destroy
+      task = Task.find_by(id: params[:id].to_i)
+      @deleted_task = task.destroy
+      redirect_to root_path
   end
-end
+
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'tasks#index'
   # get '/tasks', to: 'tasks#index'
   # get '/tasks/:id', to: 'tasks#show', as: 'task'
-  resources :tasks, only: [:index, :show, :new, :create]
+  resources :tasks
+  # delete '/tasks/:id', to: 'tasks#destroy', as :'delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
